53493a0080ab993d6a45be5536331dec2fd5d83b
Nikita Shevnin <nikita.shevnin2016@yandex.ru>
2025-07-29 21:55:11 +0300

    start init commit

---
+++ src/main/java/ru/solution/test_task_for_gitflic_team/TestTaskForGitflicTeamApplication.java
@@ -0,0 +1,13 @@
+package ru.solution.test_task_for_gitflic_team;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskForGitflicTeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTaskForGitflicTeamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/solution/test_task_for_gitflic_team/TestTaskForGitflicTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.solution.test_task_for_gitflic_team;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskForGitflicTeamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
