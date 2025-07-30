8d9f4c08d72b703f59a0058c6a3295690c4a89ee
Nikita Shevnin <nikita.shevnin2016@yandex.ru>
2025-07-30 11:45:42 +0300

    fix update SecurityConfig

--- src/main/java/ru/solution/test_task_for_gitflic_team/config/SecurityConfig.java
+++ src/main/java/ru/solution/test_task_for_gitflic_team/config/SecurityConfig.java
@@ -1,22 +1,17 @@
 package ru.solution.test_task_for_gitflic_team.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import ru.solution.test_task_for_gitflic_team.service.UserService;
 import org.springframework.security.config.Customizer;
 
 @Configuration
 @EnableMethodSecurity
-@RequiredArgsConstructor
 public class SecurityConfig {
-    private final UserService userService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -29,12 +24,6 @@ public class SecurityConfig {
             .httpBasic(Customizer.withDefaults());
 
         return http.build();
-    }
-
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        return userService;
     }
 
     @Bean
